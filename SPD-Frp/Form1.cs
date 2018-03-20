2b10cc29de32ed900377c0e3b56d651938d34f7b
l4km47 <l4km47@DESKTOP-PFB8ETA>
2018-03-20 09:44:42 +0530

    init

--- SPD-Frp/Form1.cs
+++ SPD-Frp/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Diagnostics;
 namespace SPD_Frp
 {
     public partial class Form1 : Form
@@ -16,5 +17,22 @@ namespace SPD_Frp
         {
             InitializeComponent();
         }
-    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Process process = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "";
+            process = Process.Start(startInfo);
+            process.StandardInput.WriteLine("adb devices");
+            connected_devices.Text = process.StandardOutput.ReadLine();
+
+        }
 }
